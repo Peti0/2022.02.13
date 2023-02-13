882cfd5209e7daf689407e01a0579d8dad624a7a
Peti0 <113418512+Peti0@users.noreply.github.com>
2023-02-13 15:20:50 +0100

    Add files via upload

---
+++ 1.feladat/Konzolos feladatok 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konzolos_feladatok_1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérem adjon meg egy számot!");
+            Console.WriteLine();
+            int n =Convert.ToInt32(Console.ReadLine());
+
+
+
+
+            if (n % 3 == 0)
+                Console.WriteLine("A szám osztható hárommal");
+            if (n % 4 == 0)
+                Console.WriteLine("A szám osztható négyel");
+            if (n % 9 == 0)
+                Console.WriteLine("A szám osztható kilencel");
+
+
+
+
+            Console.ReadKey();
+
+            
+        }
+        
+    }
+}
---
+++ 2.feladat/Konzolos feladatok 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konzolos_feladatok_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mennyi a hőmérséklet?");
+            Console.WriteLine();
+            int x =Convert.ToInt32(Console.ReadLine());
+
+            
+            Console.ReadKey(); 
+        }
+    }
+}
